857f5562001f5ded5487b4fe9254da507fec7d56
JoseSevla <71191978+JoseSevla@users.noreply.github.com>
2021-04-23 22:31:43 -0400

    Add files via upload

---
+++ dummy_users.dart
@@ -0,0 +1,28 @@
+import 'package:flutter_crud/models/user.dart';
+
+const DUMMY_USERS = {
+  '1': const User(
+      id: '1',
+      name: 'Maria',
+      email: 'maria@alunos.com.br',
+      avatarUrl:
+          'https://cdn.pixabay.com/photo/2021/03/03/08/56/woman-6064819_960_720.jpg'),
+  '2': const User(
+      id: '2',
+      name: 'Rafael',
+      email: 'rafael@alunos.com.br',
+      avatarUrl:
+          'https://cdn.pixabay.com/photo/2018/08/28/12/41/avatar-3637425_960_720.png'),
+  '3': const User(
+      id: '3',
+      name: 'Fernanda',
+      email: 'fernanda@zymail.com.br',
+      avatarUrl:
+          'https://cdn.pixabay.com/photo/2017/03/01/22/18/avatar-2109804_960_720.png'),
+  '4': const User(
+    id: '4',
+    name: 'Guilherme',
+    email: 'guigui@abcmail.com.br',
+    avatarUrl:
+        'https://cdn.pixabay.com/photo/2017/01/31/21/23/avatar-2027365_960_720.png'),
+};
---
+++ user_tile.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_crud/models/user.dart';
+
+class UserTile extends StatelessWidget {
+  final User user;
+
+  const UserTile(this.user);
+
+  @override
+  Widget build(BuildContext context) {
+    final avatar = user.avatarUrl == null || user.avatarUrl.isEmpty
+        ? CircleAvatar(child: Icon(Icons.person))
+        : CircleAvatar(backgroundImage: NetworkImage(user.avatarUrl));
+    return ListTile(
+        leading: avatar,
+        title: Text(user.name),
+        subtitle: Text(user.email),
+        trailing: Container(
+          width: 100,
+          child: Row(children: <Widget>[
+            IconButton(
+              icon: Icon(Icons.edit),
+              color: Colors.orange,
+              onPressed: () {},
+            ),
+            IconButton(
+              icon: Icon(Icons.delete),
+              color: Colors.red,
+              onPressed: () {
+                showDialog(
+                  context: context,
+                  builder: (ctx) => AlertDialog(
+                    title: Text('Excluir Usuário'),
+                    content: Text('Tem certeza????'),
+                    actions: <Widget>[
+                      // ignore: deprecated_member_use
+                      FlatButton(
+                        child: Text('Não'),
+                        onPressed: () => Navigator.of(context).pop(false),
+                      ),
+                      // ignore: deprecated_member_use
+                      FlatButton(
+                        child: Text('Sim'),
+                        onPressed: () => Navigator.of(context).pop(true),
+                      )
+                    ],
+                  ),
+                ).then((confirmed) {
+                  if (confirmed) {}
+                });
+              },
+            )
+          ]),
+        ));
+  }
+}
